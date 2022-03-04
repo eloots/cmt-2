a15166e847b9b1a5efe8580983bd706614e2b353
Eric Loots <eric.loots@gmail.com>
2022-03-04 15:45:48 +0100

    Refactor remainder of cmta commands
    - Moved certain methods around
    - Method `runAndExitIfFailed` on `ProcessCmd` isn't used anymore
      which means that unsuccessful process execution status is propagated
      via Either
    - Added (recursive) method on the execution of a sequence of processes
      to fail-fast
    - Moved some command specific methods to a private object
    - Added an object for test helpers (TestHelpers)
    - Renamed `ExercisePrefixesAndExerciseNames` -> `ExercisesMetadata`

--- cmta/src/main/scala/cmt/admin/command/execution/Studentify.scala
+++ cmta/src/main/scala/cmt/admin/command/execution/Studentify.scala
@@ -3,22 +3,25 @@ package cmt.admin.command.execution
 import cmt.Helpers.*
 import cmt.admin.command.AdminCommand.Studentify
 import cmt.core.execution.Executable
-import cmt.{ProcessDSL, StudentifiedSkelFolders, toConsoleGreen}
+import cmt.{StudentifiedSkelFolders, toConsoleGreen}
 import sbt.io.IO as sbtio
 import sbt.io.syntax.*
 
 given Executable[Studentify] with
   extension (cmd: Studentify)
-    def execute(): Either[String, String] = {
+    def execute(): Either[String, String] =
+      import StudentifyHelpers.*
 
       for {
-        a <- exitIfGitIndexOrWorkspaceIsntClean(cmd.mainRepository.value)
+        _ <- exitIfGitIndexOrWorkspaceIsntClean(cmd.mainRepository.value)
         _ = println(s"Studentifying ${toConsoleGreen(cmd.mainRepository.value.getPath)} to ${toConsoleGreen(
             cmd.studentifyBaseDirectory.value.getPath)}")
 
         mainRepoName = cmd.mainRepository.value.getName
         tmpFolder = sbtio.createTemporaryDirectory
-        cleanedMainRepo = ProcessDSL.copyCleanViaGit(cmd.mainRepository.value, tmpFolder, mainRepoName)
+        cleanedMainRepo = tmpFolder / cmd.mainRepository.value.getName
+
+        _ <- copyCleanViaGit(cmd.mainRepository.value, tmpFolder, mainRepoName)
 
         ExercisesMetadata(prefix, exercises, exerciseNumbers) <- getExerciseMetadata(cmd.mainRepository.value)(
           cmd.config)
@@ -38,12 +41,12 @@ given Executable[Studentify] with
           cmd,
           tmpFolder)
 
-        result <- Right(exercises.mkString("Processed exercises:\n  ", "\n  ", "\n"))
+        successMessage <- Right(exercises.mkString("Processed exercises:\n  ", "\n  ", "\n"))
 
-      } yield result
-    }
+      } yield successMessage
 
-  private def buildStudentifiedRepository(
+private object StudentifyHelpers:
+  def buildStudentifiedRepository(
       cleanedMainRepo: File,
       exercises: Vector[String],
       studentifiedRootFolder: File,
@@ -57,11 +60,18 @@ given Executable[Studentify] with
     writeStudentifiedCMTConfig(studentifiedRootFolder / cmd.config.cmtStudentifiedConfigFile, exercises)(cmd.config)
     writeStudentifiedCMTBookmark(studentifiedRootFolder / ".bookmark", exercises.head)
 
+    val successMessage = exercises.mkString("Processed exercises:\n  ", "\n  ", "\n")
     if cmd.initializeAsGitRepo.value then
       val dotIgnoreFile = cleanedMainRepo / ".gitignore"
       if dotIgnoreFile.exists then sbtio.copyFile(dotIgnoreFile, studentifiedRootFolder / ".gitignore")
-      initializeGitRepo(studentifiedRootFolder)
-      commitToGit("Initial commit", studentifiedRootFolder)
-
-    sbtio.delete(tmpFolder)
-    Right(exercises.mkString("Processed exercises:\n  ", "\n  ", "\n"))
+      for {
+        _ <- initializeGitRepo(studentifiedRootFolder)
+        _ <- commitToGit("Initial commit", studentifiedRootFolder)
+        _ = sbtio.delete(tmpFolder)
+        result <- Right(successMessage)
+      } yield result
+    else
+      sbtio.delete(tmpFolder)
+      Right(successMessage)
+  end buildStudentifiedRepository
+end StudentifyHelpers
